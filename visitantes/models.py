32a1494d83444c3629c9e830a4bb5bca6cbc060e
Luis-Felipe-N <luisj2felipe09@gmail.com>
2022-02-10 22:34:31 -0300

    db fixed up

--- visitantes/models.py
+++ visitantes/models.py
@@ -4,64 +4,61 @@ from django.db import models
 
 class Visitante(models.Model):
     nome_completo = models.CharField(
-        verbose_name="Nome completo",
-        max_length=194
+        verbose_name="Nome completo", max_length=194
     )
 
     cpf = models.CharField(
         verbose_name="CPF",
-        max_length=11
+        max_length=11,
     )
 
     data_nascimento = models.DateField(
         verbose_name="Data de nascimento",
-        auto_now_add=False,
         auto_now=False
     )
 
-    numero_casa = models.PositiveSmallIntegerField(
+    numero_casa = models.CharField(
         verbose_name="Número da casa a ser visitada",
-    )
-
-    placa_veiculo = models.CharField(
-        verbose_name="Placa do veículo",
-        max_length=11,
-        blank=True,
-        null=True
+        max_length=3,
     )
 
     horario_chegada = models.DateTimeField(
-        verbose_name="Horário de chegada do visitante",
-        auto_now_add=True
-    )
-
-    horario_saida = models.DateTimeField(
-        verbose_name="Horário de saída do visitante",
-        auto_now=False,
-        blank=True,
-        null=True
+        verbose_name="Horário de chegada na recepção", auto_now_add=True
     )
 
     horario_autorizacao = models.DateTimeField(
         verbose_name="Horário de autorização de entrada",
-        auto_now=False,
+        null=True,
         blank=True,
-        null=True
+        auto_now=False,
+    )
+
+    horario_saida = models.DateTimeField(
+        verbose_name="Horário de saída do condomínio",
+        null=True,
+        blank=True,
+        auto_now=False,
+    )
+
+    registrado_por = models.ForeignKey(
+        "porteiros.Porteiro",
+        verbose_name="Porteiro responsável pelo registro",
+        on_delete=models.CASCADE,
+    )
+
+    placa_veiculo = models.CharField(
+        verbose_name="Placa do veículo",
+        max_length=10,
+        blank=True,
+        null=True,
     )
 
     morador_responsavel = models.CharField(
-        verbose_name="Nome do morador",
+        verbose_name="Nome do morador responsável por autorizar a entrada do visitante",
         max_length=194,
-        blank=True
+        blank=True,
+        null=True,
     )
-
-
-    # registrado_por = models.ForeignKey(
-    #     "porteiros.Porteiro",
-    #     verbose_name="Porteiro responsável pelo registro",
-    #     on_delete=models.PROTECT,
-    # )
-
     class Meta:
         verbose_name = 'Visitante'
         verbose_name_plural = 'Visitantes'
--- visitantes/views.py
+++ visitantes/views.py
@@ -1,13 +1,32 @@
-from django.shortcuts import render
+from django.contrib import messages
+from multiprocessing import context
+from django.shortcuts import redirect, render
 from visitantes.forms import VisitanteForm
 
-def registrar_visitantes( request ):
 
-    form = VisitanteForm
+def registrar_visitantes(request):
+
+    form = VisitanteForm()
+
+    if request.method == 'POST':
+        form = VisitanteForm(request.POST)
+
+        if form.is_valid():
+            visitante = form.save(commit=False)
+            visitante.registrado_por = request.user.porteiro
+
+            visitante.save()
+
+            messages.success(
+                request,
+                "Visitante registrado com sucesso"
+            )
+
+            return redirect("index")
 
     context = {
-        "nome_pagina": "Registrar visitantes",
-        "form": form
+        'nome_pagina': 'Registrar visitantes',
+        'form': form
     }
 
-    return render( request , 'registrar_visitante.html', context )
+    return render( request, 'registrar_visitante.html', context)
