c312ed8911c702f6dfe43fdb32a5677f77b22485
Luis-Felipe-N <luisj2felipe09@gmail.com>
2022-02-22 16:15:16 -0300

    make it better

--- controle_visitantes/settings.py
+++ controle_visitantes/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 import sys
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +132,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "index"
+
+django_heroku.settings(locals())
